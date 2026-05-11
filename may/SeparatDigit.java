3b6d56abe7101568dd89d22132e7a7f5adc8b2b4
keshavsharma5118 <keshav.s5118@gmail.com>
2026-05-11 23:32:46 +0530

    first problem solved by own

---
+++ may/SeparatDigit.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+class Solution {
+    public int[] separateDigits(int[] nums) {
+            ArrayList<Integer> list = new ArrayList<>();
+            int k = 0;
+            // first loop created for traversing from back and digit extraction 
+            for(int i = nums.length-1; i>=0; i--){
+                while(nums[i]>0){
+                 int digit = nums[i]%10;
+                 list.add(digit);
+                 nums[i]/=10;
+                }
+            }
+            // array initialization 
+            int [] answer = new int[list.size()];
+           
+           // reversal loop and add to answer array
+            for(int j=list.size()-1; j>=0 ; j--){
+                answer[k]= list.get(j);
+                k++;
+                }
+            // return statement
+            return answer;
+    }
+}
