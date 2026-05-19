8923c8b193c330557c8e58a1f732394a2dc0095b
keshavsharma5118 <keshav.s5118@gmail.com>
2026-05-19 08:33:20 +0530

    gfg basic java

---
+++ may/GFG/ReverseArray.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isSorted(int[] arr) {
+        int n = arr.length;
+        boolean flag = false;
+    
+    // if  n = 8
+    // i - 1-> 6;
+    
+        if ( n == 1){
+        flag = true;
+        return flag;
+        }
+        for(int i =0; i<n-2; i++ ){  
+            
+            if(arr[i]<=arr[i+1])
+                {
+                   flag  = true;
+                }
+            else
+                {
+             flag = false;
+             return flag; 
+                }   
+        }
+        return flag; 
+        
+    }
+}
